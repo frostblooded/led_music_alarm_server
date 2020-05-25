c1d6a3256fa59c1dc615f9802d791b8c6a207e91
Nikolay Danailov <niki@raspberrypi>
2020-05-25 07:02:54 +0100

    Add endpoints for setting alarm
    Also restart mopidy service when credentials are set

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -6,7 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def parse_request():
+def index():
     if request.method == 'GET':
         return render_template('index.html')
 
@@ -16,7 +17,7 @@ def parse_request():
         text = '''[spotify]
 username = {}
 password = {}
-cliend_id = {}
+client_id = {}
 client_secret = {}
 private_session = true
 
@@ -30,4 +31,24 @@ enabled = true
         text_file = open("/etc/mopidy/mopidy.conf", "w")
         text_file.write(text)
         text_file.close()
-        return "Thank you!"
+
+        print("Service restart:", os.system('sudo systemctl restart mopidy.service'))
+
+        return render_template('index.html', flash="Credentials set!")
+    
+@app.route('/alarm', methods=['POST'])
+def alarm():
+    cron_text = '{} {} * * * mpc clear && mpc add {} && mpc play'.format(
+        request.form['minutes'],
+        request.form['hours'],
+        request.form['spotify_uri']
+    )
+
+    remove_cron_command = 'sudo crontab -r'
+    cron_set_command = '(sudo crontab -l ; echo "{}") | sort - | uniq - | sudo crontab -'.format(cron_text)
+
+    print("Removing old crontabs:", os.system(remove_cron_command))
+    print("Setting crontab:", os.system(cron_set_command))
+    
+    return render_template('index.html', flash="Alarm set!")
+
